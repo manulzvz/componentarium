efab7f32bd6d3f650b06bfe403818cb63abf1f13
Manuel Lopez <manulzvz@gmail.com>
2025-09-07 16:28:57 -0600

    feat: Add testimonial-card component

--- testimonial-card/src/App.tsx
+++ testimonial-card/src/App.tsx
@@ -1,5 +1,22 @@
+import TestimonialCard from "./TestimonialCard";
+import profileImg from "./assets/profile-thumbnail.png";
+
 function App() {
-  return <>Testimonial Card</>;
+  const testimonial = {
+    description:
+      "I've been searching for high-quality abstract images for my design projects, and I'm thrilled to have found this platform. The variety and depth of creativity are astounding!",
+    name: "Sarah Dole",
+    username: "sarahdole",
+    image: profileImg,
+  };
+
+  return (
+    <div className="relative">
+      <div className="h-screen w-full flex justify-center">
+        <TestimonialCard {...testimonial} />
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ testimonial-card/src/TestimonialCard.tsx
@@ -0,0 +1,39 @@
+type TestimonialCardProps = {
+  description: string;
+  name: string;
+  username: string;
+  image: string;
+};
+
+function TestimonialCard({
+  description,
+  name,
+  username,
+  image,
+}: TestimonialCardProps) {
+  return (
+    <article
+      aria-label="testimonial"
+      className="p-6 bg-white shadow space-y-4 rounded-lg max-w-[340px] max-h-[232px] mt-[200px] mx-auto"
+    >
+      <div className="flex items-center gap-4">
+        <img
+          loading="lazy"
+          src={image}
+          alt={`${name} profile image`}
+          className="size-12"
+        />
+        <div>
+          <p className="font-semibold text-lg text-neutral-900">{name}</p>
+          <p className="text-sm text-neutral-600">@{username}</p>
+        </div>
+      </div>
+
+      <blockquote>
+        <p className="text-neutral-600">{description}</p>
+      </blockquote>
+    </article>
+  );
+}
+
+export default TestimonialCard;
