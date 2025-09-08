706380e6cf709016f639637cdabe489385f6539a
Manuel Lopez <manulzvz@gmail.com>
2025-09-08 08:55:38 -0600

    fix: update build blog-card

--- blog-card/vite.config.ts
+++ blog-card/vite.config.ts
@@ -3,5 +3,5 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/componentarium/blog-card/", // 👈 base correcta para GitHub Pages
+  base: "./",
 });
